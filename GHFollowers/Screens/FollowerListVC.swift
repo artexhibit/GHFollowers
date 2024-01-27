3330424c90c7a5d0d53b5b8221a2c8163d207f1c
artexhibit <vendabal@yandex.ru>
2024-01-27 21:49:22 +0300

    refactor getFollowers method to return Result Type

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,10 +9,13 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username ?? "", page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff", message: errorMessage!.rawValue, buttonTitle: "OK")
-                return
+        NetworkManager.shared.getFollowers(for: username ?? "", page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case.failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff", message: error.rawValue, buttonTitle: "OK")
             }
         }
     }
