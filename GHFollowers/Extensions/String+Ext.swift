f7abff6141b43120473e21d3b9aa63517c84526c
artexhibit <vendabal@yandex.ru>
2024-02-04 19:56:09 +0300

    add dateLabel

---
+++ GHFollowers/Extensions/String+Ext.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension String {
+    
+    func convertToDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale = Locale.current
+        dateFormatter.timeZone = .current
+        
+        return dateFormatter.date(from: self)
+    }
+    
+    func convertToDisplayFormat() -> String {
+        guard let date = self.convertToDate() else { return "N/A" }
+        return date.convertToMonthYearFormat()
+    }
+}
